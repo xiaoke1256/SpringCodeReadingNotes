20ed2822faf32efe57d29a79f608e625a7f72116
tangjun <tangjun@CN-20170216CTDG>
2020-02-17 16:33:19 +0800

    传统方法写的Service

--- MyTest/src/main/java/com/xiaoke1256/spring/mytest/service/Test2Servce.java
+++ MyTest/src/main/java/com/xiaoke1256/spring/mytest/service/Test2Servce.java
@@ -3,24 +3,14 @@ package com.xiaoke1256.spring.mytest.service;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.xiaoke1256.spring.mytest.base.BaseDao;
 import com.xiaoke1256.spring.mytest.bo.TestTable;
 
-@Service
-@Transactional
-public class TestService {
-	
+public class Test2Servce {
 	@Autowired
 	private BaseDao baseDao;
-	
-	public void saveTest(TestTable test) {
-		test.setId(UUID.randomUUID().toString().replace("-", ""));
-		baseDao.save(test);
-	}
-	
+
 	public void saveTest(TestTable test,boolean isRollback) {
 		test.setId(UUID.randomUUID().toString().replace("-", ""));
 		baseDao.save(test);
@@ -29,7 +19,6 @@ public class TestService {
 			throw new RuntimeException("Rollback!");
 	}
 	
-	@Transactional(readOnly=true)
 	public int count() {
 		return baseDao.count(TestTable.class);
 	}
